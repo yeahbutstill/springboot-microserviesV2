ec4dfa86e516cbb5000437fc63473fbcd10da32a
yeahbutstill <mayadani28@gmail.com>
2022-08-23 16:35:43 +0700

    Invoking Currency Exchange from Conversion Service

---
+++ currency-conversion-service/src/main/java/com/yeahbutstill/currencyconversionservice/bean/CurrencyConversion.java
@@ -0,0 +1,24 @@
+package com.yeahbutstill.currencyconversionservice.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CurrencyConversion {
+
+    private String id;
+    private String from;
+    private String to;
+    private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
+    private BigDecimal totalCalculatedAmount;
+    private String environment;
+
+}
