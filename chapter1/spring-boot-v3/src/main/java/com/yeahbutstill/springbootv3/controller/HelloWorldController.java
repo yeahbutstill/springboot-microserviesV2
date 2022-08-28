b6cad94fc54000b28795b418b6c2f02c34c699e4
yeahbutstill <mayadani28@gmail.com>
2022-08-29 00:50:42 +0700

    Play with i18n

--- chapter1/spring-boot-v3/src/main/java/com/yeahbutstill/springbootv3/controller/HelloWorldController.java
+++ chapter1/spring-boot-v3/src/main/java/com/yeahbutstill/springbootv3/controller/HelloWorldController.java
@@ -1,14 +1,22 @@
 package com.yeahbutstill.springbootv3.controller;
 
 import com.yeahbutstill.springbootv3.entity.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 @RequestMapping("/yeahbutstill/api/v1/")
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -23,6 +31,20 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean("Hello " + name);
+    }
+
+    @GetMapping(path = "/hello-world-i18n")
+    public String helloWorldI18n() {
+
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        /***
+         * Example 'en' - English
+         * nl - Duct
+         * fr - French
+         * de - Deutsch
+         */
+
     }
 
 }
