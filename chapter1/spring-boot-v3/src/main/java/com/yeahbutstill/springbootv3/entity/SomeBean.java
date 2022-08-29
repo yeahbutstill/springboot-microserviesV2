6a711c1530a7d33c30892f40b6c8a519dcb731a6
yeahbutstill <mayadani28@gmail.com>
2022-08-29 12:18:32 +0700

    Play with json ignore static filtering

--- chapter1/spring-boot-v3/src/main/java/com/yeahbutstill/springbootv3/entity/SomeBean.java
+++ chapter1/spring-boot-v3/src/main/java/com/yeahbutstill/springbootv3/entity/SomeBean.java
@@ -1,5 +1,7 @@
 package com.yeahbutstill.springbootv3.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -12,6 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties({"field", "field"})
 public class SomeBean {
 
     @NotNull
--- chapter1/spring-boot-v3/src/main/java/com/yeahbutstill/springbootv3/filtering/StaticFilteringController.java
+++ chapter1/spring-boot-v3/src/main/java/com/yeahbutstill/springbootv3/filtering/StaticFilteringController.java
@@ -4,6 +4,9 @@ import com.yeahbutstill.springbootv3.entity.SomeBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class StaticFilteringController {
 
@@ -14,6 +17,22 @@ public class StaticFilteringController {
                 .field2("BBBBBBBBBBBBBB")
                 .field3("CCCCCCCCCCCCCC")
                 .build();
+    }
+
+    @GetMapping("/filtering-list")
+    private List<SomeBean> filteringList() {
+        return Arrays.asList(
+                SomeBean.builder()
+                    .field("0AAAAAAAAAAAAAAA")
+                    .field2("0BBBBBBBBBBBBBB")
+                    .field3("0CCCCCCCCCCCCCC")
+                    .build(),
+                SomeBean.builder()
+                        .field("1AAAAAAAAAAAAAAA")
+                        .field2("2BBBBBBBBBBBBBB")
+                        .field3("3CCCCCCCCCCCCCC")
+                        .build()
+        );
     }
 
 }
